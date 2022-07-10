da6bb0d20569c65c77976713a887e975362de621
DeepSandhra <109024247+DeepSandhra@users.noreply.github.com>
2022-07-10 17:02:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -54,7 +54,7 @@ _𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐌𝐞 𝐅𝐨𝐫 𝐊𝐧𝐨𝐰 𝐀𝐛�
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/Cynitebots)</code>
+<code>[Button Text](buttonurl:https://t.me/MoRe_BoTs)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
